93363d50313e5ffdf60108f9b3acf9ad79615e09
UnSkilledHacker <162636192+UnSkilledHacker@users.noreply.github.com>
2024-03-07 23:04:43 +0000

    light.py

---
+++ light.py.py
@@ -0,0 +1,22 @@
+import time 
+import random 
+
+print("Do you want to hack your lights off?")
+input("Yes:")
+
+
+
+print("Exploiting.")
+time.sleep(3)
+
+print("Exploiting..")
+time.sleep(3)
+
+print("Exploiting...")
+time.sleep(3)
+
+print("Exploit was successful")
+time.sleep(1)
+
+print("Light should be off now")
+
